124074753efcc99503e348094dd77129d4754949
GERARD Alexandre <agerard57@protonmail.com>
2022-07-03 06:53:57 +0200

    feat: Deploy to heroku pt.1

--- client/src/landingPage/components/LandingPage.tsx
+++ client/src/landingPage/components/LandingPage.tsx
@@ -1,12 +1,24 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 import { BlueBorderBackground } from "../../core";
 
-export const LandingPage: FC = () => (
+export const LandingPage: FC = () => {
+  const [helloWorld, setHelloWorld] = useState("");
+  useEffect(() => {
+    const url= `${process.env.REACT_APP_API_URL}/hello`;
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>{
+      console.log(data);
+      setHelloWorld(data.message)});
+    },[]);
+  return(
   <>
     <BlueBorderBackground backgroundColor={"white"}>
       <hr />
       <h1>Hello there</h1>
+      <p>{helloWorld}</p>
     </BlueBorderBackground>
   </>
-);
+)
+}
--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,24 @@
-import express from 'express';
+import cors from "cors";
+import express from "express";
+
+require("dotenv").config();
 
 const port = process.env.PORT || 3200;
 
 const app = express();
 
+app.use(cors());
+
 // Start server
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-    });
+  console.log(`Server started on port ${port}`);
+});
 
-    //route
-    app.get('/', (req, res) => {
-        res.send('Hello World');
-    }   );
+app.get('/', (req, res) => { res.send('Hello from Express!')
+
+//route
+app.get("/hello", (req, res) => {
+  res.status(200).json({
+    message: "Hello World",
+  });
+});

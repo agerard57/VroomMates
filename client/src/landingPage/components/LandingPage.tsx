37b73fa76ddff0dd507d4e789af871c877acef9f
GERARD Alexandre <agerard57@protonmail.com>
2022-07-28 13:21:34 +0200

    WIP: Start landing page

--- client/src/landingPage/components/LandingPage.tsx
+++ client/src/landingPage/components/LandingPage.tsx
@@ -1,24 +1,63 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { FC, useEffect, useState } from "react";
 
-import { BlueBorderBackground } from "../../core";
+import { Input, RoundedContour } from "../../core";
+import HomeImage1 from "../assets/images/home-image-1.png";
 
 export const LandingPage: FC = () => {
   const [helloWorld, setHelloWorld] = useState("");
   useEffect(() => {
-    const url= `${process.env.REACT_APP_API_URL}/user`;
+    const url = `${process.env.REACT_APP_API_URL}/hello`;
     fetch(url)
-    .then((res)=>res.json())
-    .then((data)=>{
-      console.log(data[0].name.first_name);
-      setHelloWorld(data[0].name.first_name)});
-    },[]);
-  return(
-  <>
-    <BlueBorderBackground backgroundColor={"white"}>
-      <hr />
-      <h1>Hello there</h1>
-      <p>{helloWorld}</p>
-    </BlueBorderBackground>
-  </>
-)
-}
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setHelloWorld(data.message);
+      });
+  }, []);
+  return (
+    <>
+      <div
+        css={css`
+          background-color: #5096ff;
+          height: 100vh;
+        `}
+      >
+        <h1
+          css={css`
+            font-family: "Baloo2";
+            font-weight: 700;
+            color: white;
+          `}
+        >
+          Ready to go on a trip?
+        </h1>
+        <img
+          src={HomeImage1}
+          alt="home-image-1"
+          css={css`
+            width: 80%;
+          `}
+        />
+        <RoundedContour>
+          <div>
+            <h2
+              css={css`
+                font-family: "Baloo2";
+                font-weight: 550;
+                color: black;
+              `}
+            >
+              Let's find you a ride then!
+            </h2>
+            <hr />
+            <Input inputTitle="Single Trip" />
+          </div>
+        </RoundedContour>
+      </div>
+      hellerzofozejfozejfzefjpomklop
+    </>
+  );
+};
+

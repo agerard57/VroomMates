23bcc20b824903a9ebd44b1b0c1270acd47dfdcb
GERARD Alexandre <agerard57@protonmail.com>
2022-07-28 13:22:08 +0200

    feat: Add rounded countour

---
+++ client/src/core/components/RoundedContour.tsx
@@ -0,0 +1,17 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { FC } from "react";
+
+export const RoundedContour: FC = ({ children }) => (
+  <div
+    css={css`
+      background: #ffffff;
+      box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.25);
+      border-radius: 10px;
+      padding: 10%;
+      margin: 5vw;
+    `}
+  >
+    {children}
+  </div>
+);

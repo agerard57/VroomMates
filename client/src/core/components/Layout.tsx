212390d3ffd18eca180f0f142adff273394a09bb
GERARD Alexandre <agerard57@protonmail.com>
2022-07-28 14:39:26 +0200

    feat: Add navbar

--- client/src/core/components/Layout.tsx
+++ client/src/core/components/Layout.tsx
@@ -1,11 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import { FC } from "react";
 
+import { Navbar } from "./navbar";
 import { Header } from "./header";
 
 export const Layout: FC = ({ children }) => (
   <>
     <Header />
     <div className="App">{children}</div>
+    <Navbar />
   </>
 );

d8d148b46b2e342c1c7d22d6ab302a8cc456b987
GERARD Alexandre <agerard57@protonmail.com>
2022-08-03 02:02:11 +0200

    fix: Remove capital letter from folder

--- client/src/landingPage/components/LandingPage.tsx
+++ client/src/landingPage/components/LandingPage.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 import { DescriptionSection } from "./DescriptionSection";
-import { StatsSection } from "./StatsSection";
+import { StatsSection } from "./statsSection";
 import { LandingSection } from "./landingSection";
 
 export const LandingPage: FC = () => (

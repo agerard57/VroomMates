53a892b40ef242557a1e4e25b2666726a8ae1acf
GERARD Alexandre <agerard57@protonmail.com>
2022-08-11 15:51:11 +0200

    feat: Add core hook

---
+++ client/src/core/hooks/useGeolocation.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useGeolocated } from "react-geolocated";
+
+type GeolocationManager = () => {
+  coordinates: number[];
+  address: string;
+};
+
+export const useGeolocation: GeolocationManager = () => {
+  const { coords } = useGeolocated();
+  const [address, setAddress] = useState("");
+  const [coordinates, setCoordinates] = useState<number[]>([]);
+
+  if (coords && !coordinates.length) {
+    const { latitude, longitude } = coords;
+    setCoordinates([latitude, longitude]);
+  }
+
+  useEffect(() => {
+    if (coordinates.length) {
+      const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${coordinates[0]},${coordinates[1]}.json?access_token=${process.env?.REACT_APP_MAPBOX_TOKEN}&limit=1`;
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          const [{ place_name }] = data.features;
+          setAddress(place_name);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [coordinates]);
+
+  return {
+    coordinates: coordinates,
+    address,
+  };
+};

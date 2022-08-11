95d9d8e09e38d487b4f1f2f48c78beb7d1b225b1
GERARD Alexandre <agerard57@protonmail.com>
2022-08-11 15:54:59 +0200

    feat: Change toaster in menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,39 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { FC, Suspense } from "react";
-import { ToastProvider } from "react-toast-notifications";
+import { Toaster } from "react-hot-toast";
 
 import "./App.css";
 import { LoadingScreen } from "./loadingScreen";
 
 const RouteManager = React.lazy(() => import("./routeManager"));
 
-export const App: FC = () => (
-  <Suspense fallback={<LoadingScreen />}>
-    <ToastProvider>
+export const App: FC = () => {
+  return (
+    <Suspense fallback={<LoadingScreen />}>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerStyle={{ marginTop: "8vh" }}
+        toastOptions={{
+          duration: 5000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+
+          success: {
+            duration: 3000,
+            theme: {
+              primary: "green",
+              secondary: "black",
+            },
+          },
+        }}
+      />
+      {/* TODO Make component */}
       <RouteManager />
-    </ToastProvider>
-  </Suspense>
-);
+    </Suspense>
+  );
+};

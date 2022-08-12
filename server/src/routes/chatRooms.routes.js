d53dde05ff5dbc418dd9526c54bb41d869b9f1cb
GERARD Alexandre <agerard57@protonmail.com>
2022-08-12 15:36:05 +0200

    feat: Remove useless files

--- server/src/routes/chatRooms.routes.js
+++
@@ -1,7 +0,0 @@
-const chatRoomsController = require("../controllers/chatRooms.controller");
-
-module.exports = function (app) {
-  app.route("/chats").get(chatRoomsController.getAll);
-
-  app.route("/chat/:id").get(chatRoomsController.getOneByTripId);
-};

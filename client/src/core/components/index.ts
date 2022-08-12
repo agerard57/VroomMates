e6b3682489de92434a92cac9cc54bc67582ff9ca
GERARD Alexandre <agerard57@protonmail.com>
2022-08-12 15:31:29 +0200

    feat: Export Stars

--- client/src/core/components/index.ts
+++ client/src/core/components/index.ts
@@ -5,3 +5,4 @@ export { Input } from "./Input";
 export { Button } from "./Button";
 export { ProfilePic } from "./ProfilePic";
 export { ColoredBackground } from "./ColoredBackground";
+export { Stars } from "./stars";

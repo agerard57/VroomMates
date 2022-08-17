36269c4846ac6e56e8d05a4f12dbed1239e2a526
GERARD Alexandre <agerard57@protonmail.com>
2022-08-17 23:32:13 +0200

    feat: Update controller

--- server/src/controllers/profilePage.controller.js
+++ server/src/controllers/profilePage.controller.js
@@ -17,29 +17,31 @@ exports.getUserById = (req, res) => {
     })
     .lean()
     .then((user) => {
-      const ratings = user.ratings.map((rating) => parseInt(rating.rating));
       user.name.last_name = user.name.last_name.charAt(0) + ".";
-      user.ratings.forEach((rating) => {
-        rating.author.name.last_name =
-          rating.author.name.last_name.charAt(0) + ".";
-      });
-      user.avg_rating =
-        ratings.reduce((acc, curr) => acc + curr, 0) / ratings.length;
+      //Check if user.ratings exists and not empty
+      if (user.ratings && user.ratings.length > 0) {
+        const ratings = user.ratings.map((rating) => parseInt(rating.rating));
+        user.ratings.forEach((rating) => {
+          rating.author.name.last_name =
+            rating.author.name.last_name.charAt(0) + ".";
+        });
+        user.avg_rating =
+          ratings.reduce((acc, curr) => acc + curr, 0) / ratings.length;
+      }
       TripsModel.find({ driver: user._id }).then((trips) => {
         user.nb_trips_created = trips.length;
-        // Count the number of trips that the user has been a passenger in
+        // Count the number of trips that the user has been a passenger to
         user.nb_trips_participated = trips.filter((trip) => {
           return trip.passengers.includes(user._id);
         }).length;
-        if (user.status === "driver") {
-          DriversModel.find({ user: user._id }).then((driver) => {
+        DriversModel.find({ user: user._id }).then((driver) => {
+          // Check if there's a result
+          if (driver.length > 0) {
             user.car = driver[0].car;
+          }
 
-            res.status(200).json(user);
-          });
-        } else {
           res.status(200).json(user);
-        }
+        });
       });
     })
     .catch((err) => {

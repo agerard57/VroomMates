5689a230bfbd6ca68a1c7a8ee5cde6da409d7a37
agerard57 <agerard57@protonmail.com>
2022-08-21 05:07:11 +0200

    feat: Populate controller results

--- server/src/controllers/tripsPage.controller.js
+++ server/src/controllers/tripsPage.controller.js
@@ -1,6 +1,13 @@
 const TripsModel = require("../models/trips.model");
 exports.getUserTrips = (req, res) => {
   const userId = req.params.id;
+
+  const populateDriver = {
+    path: "driver",
+    model: "Users",
+    select: "_id name.first_name",
+  };
+
   const findInDriversAndPassengers = {
     $or: [
       {
@@ -38,31 +45,37 @@ exports.getUserTrips = (req, res) => {
       sortByDepartureTime,
     },
     returnFields
-  ).exec((_err, tripsCreated) => {
-    // The second one is called currentTrips and has all the trips where status="ongoing" or status="confirmed".
-    TripsModel.find(
-      {
-        findInDriversAndPassengers,
-        status: { $in: ["ongoing", "confirmed"] },
-        sortByDepartureTime,
-      },
-      returnFields
-    ).exec((_err, currentTrips) => {
-      // The third one is called pastTrips and has all the trips where status="finished"
+  )
+    .populate(populateDriver)
+    .exec((_err, tripsCreated) => {
+      // The second one is called currentTrips and has all the trips where status="ongoing" or status="confirmed".
       TripsModel.find(
         {
           findInDriversAndPassengers,
-          status: "finished",
+          status: { $in: ["ongoing", "confirmed"] },
           sortByDepartureTime,
         },
         returnFields
-      ).exec((_err, pastTrips) => {
-        res.status(200).json({
-          tripsCreated,
-          currentTrips,
-          pastTrips,
+      )
+        .populate(populateDriver)
+        .exec((_err, currentTrips) => {
+          // The third one is called pastTrips and has all the trips where status="finished"
+          TripsModel.find(
+            {
+              findInDriversAndPassengers,
+              status: "finished",
+              sortByDepartureTime,
+            },
+            returnFields
+          )
+            .populate(populateDriver)
+            .exec((_err, pastTrips) => {
+              res.status(200).json({
+                tripsCreated,
+                currentTrips,
+                pastTrips,
+              });
+            });
         });
-      });
     });
-  });
 };

d6d0f693fab7839509c41ea7718f0761ce9d5416
GERARD Alexandre <agerard57@protonmail.com>
2022-09-03 16:46:19 +0200

    WIP: Working on form

--- client/src/signInPage/components/SignInBox.tsx
+++ client/src/signInPage/components/SignInBox.tsx
@@ -6,7 +6,7 @@ import FacebookLogin from "react-facebook-login";
 import GoogleLogin from "react-google-login";
 import { useTranslation } from "react-i18next";
 
-import { Input, RoundedContour, Button } from "../../core";
+import { Inputs, RoundedContour } from "../../core";
 import facebookIcon from "../assets/icons/facebookIcon.svg";
 import googleIcon from "../assets/icons/googleIcon.svg";
 import mailIcon from "../assets/icons/mailIcon.svg";
@@ -23,73 +23,63 @@ export const SignInBox: FC = () => {
           padding: 1rem;
           `}
     >
-      <GoogleLogin
-        clientId="374277909154-56jflke52kogao5omqr5l29k1mpdukgc.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={"single_host_origin"}
-      />
-      <FacebookLogin
-        appId="1521176028311446"
-        autoLoad={true}
-        fields="public_profile,name,email,picture"
-        onClick={responseGoogle}
-        callback={responseGoogle}
-      />
-      ,
-      <Row>
-        <h2
-          css={css`
-            font-family: "Baloo2";
-            font-weight: 500;
-            text-align: center;
-            margin-bottom: 0;
-          `}
-        >
-          {t("signInSection.title")}
-        </h2>
-      </Row>
-      <hr />
-      <Row>
-        <Col>
-          <Input
-            inputName="googleSignIn"
-            inputType="radio"
-            inputPlaceholder="Google"
-            icon={googleIcon}
+      <form
+        action={`${process.env.REACT_APP_API_URL}/profile/login`}
+        method="post"
+      >
+        <Row>
+          <h2
+            css={css`
+              font-family: "Baloo2";
+              font-weight: 500;
+              text-align: center;
+              margin-bottom: 0;
+            `}
+          >
+            {t("signInSection.title")}
+          </h2>
+        </Row>
+        <hr />
+        <Row>
+          <Col>
+            <GoogleLogin
+              clientId="374277909154-56jflke52kogao5omqr5l29k1mpdukgc.apps.googleusercontent.com"
+              buttonText="Login"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
+            />
+          </Col>
+          <Col>
+            <FacebookLogin
+              appId="1521176028311446"
+              autoLoad={true}
+              fields="public_profile,name,email,picture"
+              onClick={responseGoogle}
+              callback={responseGoogle}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Inputs.TextInput
+            inputName="email"
+            inputPlaceholder={t("signInSection.email")}
+            icon={mailIcon}
           />
-        </Col>
-        <Col>
-          <Input
-            inputName="facebookSignIn"
-            inputType="radio"
-            inputPlaceholder="Facebook"
-            icon={facebookIcon}
+        </Row>
+        <Row>
+          <Inputs.TextInput
+            inputName="password"
+            inputPlaceholder={t("signInSection.password")}
+            icon={passwordIcon}
           />
-        </Col>
-      </Row>
-      <Row>
-        <Input
-          inputName="arrivalLocation"
-          inputType="text"
-          inputPlaceholder={t("signInSection.email")}
-          icon={mailIcon}
-        />
-      </Row>
-      <Row>
-        <Input
-          inputName="date"
-          inputType="text"
-          inputPlaceholder={t("signInSection.password")}
-          icon={passwordIcon}
-        />
-      </Row>
-      <Row>
-        <Button type="primary" buttonType="submit">
-          {t("signInSection.signInButton")}
-        </Button>
-      </Row>
+        </Row>
+        <Row>
+          <Inputs.Button type="primary" buttonType="submit">
+            {t("signInSection.signInButton")}
+          </Inputs.Button>
+        </Row>
+      </form>
     </RoundedContour>
   );
 };

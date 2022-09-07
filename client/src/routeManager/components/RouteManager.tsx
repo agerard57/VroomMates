97bbb9e40bb7a9c66248ccb31025275932afe131
GERARD Alexandre <agerard57@protonmail.com>
2022-09-07 03:01:44 +0200

    feat: Protect routes

--- client/src/routeManager/components/RouteManager.tsx
+++ client/src/routeManager/components/RouteManager.tsx
@@ -18,10 +18,10 @@ import { SignInPage } from "../../signInPage";
 import { TripDetailsPage } from "../../tripDetailsPage";
 import { TripsPage } from "../../tripsPage";
 import { useRouteManager } from "../hooks";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 const RouteManager: FC = () => {
   const { isUserLoggedIn, loggedUserData, isLoading } = useRouteManager();
-  console.error(isUserLoggedIn, loggedUserData); // TODO Remove
 
   if (isLoading) return <LoadingScreen />;
 
@@ -30,16 +30,54 @@ const RouteManager: FC = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/dashboard" element={<Dashboard loggedUserData={loggedUserData} />} />
+          <Route
+            path="/dashboard"
+            element={<Dashboard loggedUserData={loggedUserData} />}
+          />
           <Route path="/home" element={<LandingPage />} />
-          <Route path="/user/:id" element={<ProfilePage />} />
+          <Route
+            path="/user/:id"
+            element={
+              <ProtectedRoute isAllowed={isUserLoggedIn}>
+                <ProfilePage loggedUserData={loggedUserData} />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/policies" element={<PoliciesPages />} />
-          <Route path="/profile/view" element={<ProfilePage />} />
+          <Route
+            path="/profile/view"
+            element={
+              <ProtectedRoute isAllowed={isUserLoggedIn}>
+                <ProfilePage loggedUserData={loggedUserData} />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/trip/:id" element={<TripDetailsPage />} />
-          <Route path="/trips" element={<TripsPage />} />
-          <Route path="/reviews/given" element={<ReviewsPages />} />
-          <Route path="/reviews/received" element={<ReviewsPages />} />
+          <Route
+            path="/trips"
+            element={
+              <ProtectedRoute isAllowed={isUserLoggedIn}>
+                <TripsPage loggedUserData={loggedUserData} />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/reviews/given"
+            element={
+              <ProtectedRoute isAllowed={isUserLoggedIn}>
+                <ReviewsPages />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/reviews/received"
+            element={
+              <ProtectedRoute isAllowed={isUserLoggedIn}>
+                <ReviewsPages />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/profile/login" element={<SignInPage />} />
         </Routes>
       </Layout>

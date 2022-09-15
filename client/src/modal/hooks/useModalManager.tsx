40aded24b6f05b81cd5b196ef9979e950d683741
GERARD Alexandre <agerard57@protonmail.com>
2022-09-15 06:40:49 +0200

    feat: Add logic

---
+++ client/src/modal/hooks/useModalManager.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+import { SlideSpec, UseModalManager } from "../types";
+
+export const useModalManager = (): UseModalManager => {
+  const emptySlideSpec = [{ content: <></>, nextButtonText: "" }];
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [slideSpecs, setSlideSpecs] = useState<SlideSpec[]>(emptySlideSpec);
+  const [slideNumber, setSlideNumber] = useState<number>(0);
+
+  const openModal = (slideSpecs: SlideSpec[]) => {
+    setSlideNumber(0);
+    setSlideSpecs(slideSpecs);
+    setIsOpen(true);
+  };
+
+  const previous = () =>
+    slideNumber === 0 ? closeModal() : setSlideNumber(slideNumber - 1);
+  const last = slideSpecs.length - 1;
+  const next = () =>
+    slideNumber === last ? closeModal() : setSlideNumber(slideNumber + 1);
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setSlideSpecs(emptySlideSpec);
+    setSlideNumber(0);
+  };
+
+  return {
+    isOpen,
+    slideSpecs,
+    openModal,
+    closeModal,
+    iterator: { previous, slideNumber, next },
+  };
+};

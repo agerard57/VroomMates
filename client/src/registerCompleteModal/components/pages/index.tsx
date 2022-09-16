108c4f603911d5af1becf6bde104ddd5b42df41a
GERARD Alexandre <agerard57@protonmail.com>
2022-09-16 22:45:29 +0200

    feat: Export them all

---
+++ client/src/registerCompleteModal/components/pages/index.tsx
@@ -0,0 +1,4 @@
+export { FirstMessagePage } from "./FirstMessagePage";
+export { SecondProfilePic } from "./SecondProfilePic";
+export { ThirdAbout } from "./ThirdAbout";
+export { FourthAllSet } from "./FourthAllSet";

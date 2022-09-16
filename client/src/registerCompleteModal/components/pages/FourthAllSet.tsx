7874e0969bcf15212eda0eb59f6297f6e0bcb4d8
GERARD Alexandre <agerard57@protonmail.com>
2022-09-16 22:45:19 +0200

    feat: Add fourth page (no logic)

---
+++ client/src/registerCompleteModal/components/pages/FourthAllSet.tsx
@@ -0,0 +1,59 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { FC } from "react";
+import { Container, Row } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+
+import AllSetImageSrc from "../../assets/images/allSetImage.jpg";
+
+export const FourthAllSet: FC = () => {
+  const { t } = useTranslation("RegisterCompleteModal");
+
+  return (
+    <Container
+      css={css`
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+      `}
+    >
+      <Row>
+        <img
+          src={AllSetImageSrc}
+          alt="register pic"
+          css={css`
+            width: 60vw;
+            margin-left: auto;
+            margin-right: auto;
+            display: block;
+            padding-bottom: 1rem;
+          `}
+        />
+        <h1
+          css={css`
+            font-weight: 700;
+            color: #3d3d3d;
+          `}
+        >
+          {t("page.3.title")}
+        </h1>
+      </Row>
+      <Row
+        css={css`
+          display: flex;
+        `}
+      >
+        <p
+          css={css`
+            color: #636363;
+            padding-top: 1rem;
+            text-align: center;
+            white-space: pre-wrap;
+          `}
+        >
+          {t("page.3.body")}
+        </p>
+      </Row>
+    </Container>
+  );
+};

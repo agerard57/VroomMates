62bc0e1c570c98c0316d8ba54ccadd56bcbca29e
GERARD Alexandre <agerard57@protonmail.com>
2022-09-19 12:59:08 +0200

    feat: Remove former searchBar

--- client/src/core/components/index.ts
+++ client/src/core/components/index.ts
@@ -10,4 +10,4 @@ export { MenuListItem } from "./MenuListItem";
 export { ReviewsCard } from "./reviews";
 export { ProfileHeader, ProfileBanner } from "./profileBanner";
 export { Toaster } from "./Toaster";
-export { SearchBox } from "./SearchBox";
+export { SearchBox } from "./searchBox";

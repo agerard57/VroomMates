94544ae069b4f94508cbe961d0542fc95d1cb4eb
GERARD Alexandre <agerard57@protonmail.com>
2022-09-20 09:12:52 +0200

    feat: Wrap routes with error handler

--- client/src/routeManager/components/RouteManager.tsx
+++ client/src/routeManager/components/RouteManager.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import {
   BrowserRouter as Router,
   Route,
@@ -8,6 +9,7 @@ import {
 
 import { Layout } from "../../core";
 import { Dashboard } from "../../dashboard";
+import { ErrorPage } from "../../errorPage";
 import { LandingPage } from "../../landingPage";
 import { LoadingScreen } from "../../loadingScreen";
 import { PoliciesPages } from "../../policiesPages";
@@ -29,59 +31,61 @@ const RouteManager: FC = () => {
   return (
     <Router>
       <Layout>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route
-            path="/dashboard"
-            element={<Dashboard loggedUserData={loggedUserData} />}
-          />
-          <Route path="/home" element={<LandingPage />} />
-          <Route
-            path="/user/:id"
-            element={
-              <ProtectedRoute isAllowed={isUserLoggedIn}>
-                <ProfilePage loggedUserData={loggedUserData} />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/policies" element={<PoliciesPages />} />
-          <Route
-            path="/profile/view"
-            element={
-              <ProtectedRoute isAllowed={isUserLoggedIn}>
-                <ProfilePage loggedUserData={loggedUserData} />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/trip/:id" element={<TripDetailsPage />} />
-          <Route
-            path="/trips"
-            element={
-              <ProtectedRoute isAllowed={isUserLoggedIn}>
-                <TripsPage loggedUserData={loggedUserData} />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/profile/register" element={<RegisterPage />} />
-          <Route
-            path="/reviews/given"
-            element={
-              <ProtectedRoute isAllowed={isUserLoggedIn}>
-                <ReviewsPages loggedUserData={loggedUserData} />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/reviews/received"
-            element={
-              <ProtectedRoute isAllowed={isUserLoggedIn}>
-                <ReviewsPages loggedUserData={loggedUserData} />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/profile/login" element={<SignInPage />} />
-        </Routes>
+        <ErrorBoundary FallbackComponent={ErrorPage}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route
+              path="/dashboard"
+              element={<Dashboard loggedUserData={loggedUserData} />}
+            />
+            <Route path="/home" element={<LandingPage />} />
+            <Route
+              path="/user/:id"
+              element={
+                <ProtectedRoute isAllowed={isUserLoggedIn}>
+                  <ProfilePage loggedUserData={loggedUserData} />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/policies" element={<PoliciesPages />} />
+            <Route
+              path="/profile/view"
+              element={
+                <ProtectedRoute isAllowed={isUserLoggedIn}>
+                  <ProfilePage loggedUserData={loggedUserData} />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/trip/:id" element={<TripDetailsPage />} />
+            <Route
+              path="/trips"
+              element={
+                <ProtectedRoute isAllowed={isUserLoggedIn}>
+                  <TripsPage loggedUserData={loggedUserData} />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/profile/register" element={<RegisterPage />} />
+            <Route
+              path="/reviews/given"
+              element={
+                <ProtectedRoute isAllowed={isUserLoggedIn}>
+                  <ReviewsPages loggedUserData={loggedUserData} />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/reviews/received"
+              element={
+                <ProtectedRoute isAllowed={isUserLoggedIn}>
+                  <ReviewsPages loggedUserData={loggedUserData} />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/profile/login" element={<SignInPage />} />
+          </Routes>
+        </ErrorBoundary>
       </Layout>
     </Router>
   );

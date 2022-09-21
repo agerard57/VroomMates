2e8a265706f969e5fdeed09db542acadfd04dec7
GERARD Alexandre <agerard57@protonmail.com>
2022-09-22 00:18:19 +0200

    feat: Move calendar down to core

--- client/src/landingPage/assets/icons/index.ts
+++ client/src/landingPage/assets/icons/index.ts
@@ -1,5 +1,4 @@
 import ArrivalIcon from "./arrivalIcon.svg";
-import CalendarIcon from "./calendarIcon.svg";
 import DepartureIcon from "./departureIcon.svg";
 
-export { ArrivalIcon, CalendarIcon, DepartureIcon };
+export { ArrivalIcon, DepartureIcon };

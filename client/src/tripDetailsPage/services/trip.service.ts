a58d0df5735269a52efbce92d4da12f3f3c25003
GERARD Alexandre <agerard57@protonmail.com>
2022-09-24 01:38:38 +0200

    WIP: Work on axios

--- client/src/tripDetailsPage/services/trip.service.ts
+++ client/src/tripDetailsPage/services/trip.service.ts
@@ -1,9 +1,11 @@
+import axios from "axios";
+
 const url = (id: string) => `${process.env?.REACT_APP_API_URL}/trip/${id}`;
 
 export const getTrip = async (id: string) => {
   try {
-    const response = await fetch(url(id));
-    return await response.json();
+    const response = await axios.get(url(id));
+    return await response.data;
   } catch (error) {
     return [];
   }

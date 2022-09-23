b730719922e3893718640be77c0ab69a58b73367
GERARD Alexandre <agerard57@protonmail.com>
2022-09-24 01:36:27 +0200

    WIP: Add axios

---
+++ client/src/core/services/privateAxios.service.ts
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+import { cookiesManager } from "../helpers";
+import { tokenService } from "./token.service";
+
+const authToken = cookiesManager.getCookie("authToken");
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+axios.interceptors.request.use(
+  async (config) => {
+    if (authToken) {
+      config.headers = {
+        ...config.headers,
+        "x-access-token": authToken,
+      };
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const config = error?.config;
+
+    if (error?.response?.status === 401 && !config?.sent) {
+      config.sent = true;
+
+      const result = await tokenService.refreshAuthToken();
+
+      if (result?.accessToken) {
+        config.headers = {
+          ...config.headers,
+          "x-access-token": authToken,
+        };
+      }
+
+      return axios(config);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export const privateAxios = axios;

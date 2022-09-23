6aea27790778b5a36603a05aa8b4239a02b41ce4
GERARD Alexandre <agerard57@protonmail.com>
2022-09-23 21:24:09 +0200

    feat: Update placeName normalizer

--- client/src/core/helpers/normalizePlaceName.ts
+++ client/src/core/helpers/normalizePlaceName.ts
@@ -12,26 +12,28 @@ export const normalizePlaceName: NormalizePlaceName = (
   lng = "en"
 ) => {
   if (placeName.length > 0) {
+    //Count the number if segments separated by commas
+    const segments = placeName.split(",");
+    // If there are 3 or less segments, we assume that the first segment is the city name
+    const city = segments.length <= 3 ? segments[0] : segments[1];
+
     switch (format) {
       case "full":
-        return placeName
-          .substring(0, placeName.lastIndexOf(","))
-          .replace(/\d/g, "")
-          .replace("  ", " ");
+        return placeName.slice(0, placeName.length);
       case "near":
-        const near = lng === "en" ? "near" : "proche de";
-        return (
-          near +
-          placeName
-            .substring(0, placeName.lastIndexOf(","))
-            .replace(/\d/g, "")
-            .replace("  ", " ")
-        );
+        const near =
+          segments.length <= 3
+            ? lng === "en"
+              ? "in "
+              : "dans "
+            : lng === "en"
+            ? "near "
+            : "proche de ";
+        return near + segments[0].replace(/\d/g, "");
+
       case "city":
-        return placeName
-          .split(",")
-          [placeName.split(",").length - 2].split(" ")
-          .pop()!;
+        return city.charAt(0) === " " ? city.slice(1) : city;
+
       default:
         return placeName;
     }

bdb90621128262c56d92056012f46dd5c6a683ed
GERARD Alexandre <agerard57@protonmail.com>
2022-09-25 14:38:38 +0200

    fix: Fix registerComplete unauthorized calls

--- client/src/registerCompleteModal/services/about.service.ts
+++ client/src/registerCompleteModal/services/about.service.ts
@@ -1,12 +1,12 @@
-import { privateAxios } from "../../core";
+import { cookiesManager, privateAxios } from "../../core";
 import { AboutInputs } from "../types";
 
 const url = `${process.env?.REACT_APP_API_URL}/profile/edit/about`;
 
 export const postAbout = async (inputs: AboutInputs) => {
   try {
-    const response = await privateAxios.put(url, {
-      body: JSON.stringify(inputs),
+    const response = await privateAxios.put(url, inputs, {
+      headers: { "x-access-token": cookiesManager.getCookie("authToken") },
     });
     const data = await response.data;
     data.status = response.status;
--- client/src/registerCompleteModal/services/profilePic.service.ts
+++ client/src/registerCompleteModal/services/profilePic.service.ts
@@ -1,4 +1,4 @@
-import { privateAxios } from "../../core";
+import { cookiesManager, privateAxios } from "../../core";
 
 const url = `${process.env?.REACT_APP_API_URL}/photo/upload`;
 
@@ -6,7 +6,9 @@ export const postProfilePic = async (file: File) => {
   const formData = new FormData();
   formData.append("file", file);
   try {
-    const response = await privateAxios.post(url, formData);
+    const response = await privateAxios.post(url, formData, {
+      headers: { "x-access-token": cookiesManager.getCookie("authToken") },
+    });
     const data = await response.data;
     data.status = response.status;
 

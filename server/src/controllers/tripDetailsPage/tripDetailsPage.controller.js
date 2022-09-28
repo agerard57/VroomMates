0ab1ddd7fbd8a035823eaa85ab5c47d55f27617f
GERARD Alexandre <agerard57@protonmail.com>
2022-09-29 01:23:50 +0200

    refacto: Move onto it's own folder

---
+++ server/src/controllers/tripDetailsPage/tripDetailsPage.controller.js
@@ -0,0 +1,9 @@
+const cancelTrip = require("./cancelTrip.controller");
+const getTripDetailsById = require("./getTripDetailsById.controller");
+const removePassenger = require("./removePassenger.controller");
+
+module.exports = {
+  cancelTrip,
+  getTripDetailsById,
+  removePassenger,
+};
